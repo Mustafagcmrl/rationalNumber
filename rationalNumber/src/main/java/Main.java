9ebfe42eeb0e619a158ed22e673cf8da9370e344
Mustafa Gocamarlı <95622468+Mustafagcmrl@users.noreply.github.com>
2023-01-16 13:44:15 +0300

    Add files via upload

--- rationalNumber/src/main/java/Main.java
+++ rationalNumber/src/main/java/Main.java
@@ -2,10 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
         Rational rational = new Rational(8,9);
-        System.out.println(rational.getResult());
 
-        Rational rational1 = new Rational(8,0);
+        System.out.println("8/9 = " + rational.getResult());
+        rational.SetNumerator(72);
+        rational.SetDenominator(16);
+        System.out.println("72/16 = " + rational.getResult());
+
+        System.out.println(rational + "\n");
+            
+        
+        Rational rational1 = new Rational(16,5);
         System.out.println(rational1.getResult());
+        System.out.println(rational1);
+        System.out.println(rational.compareTo(rational1));
+
 
     }
 
