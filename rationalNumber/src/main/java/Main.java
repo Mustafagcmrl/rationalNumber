103c5f99f7b61d2163c8e18a326a35b29bc17ff5
Mustafa Gocamarlı <95622468+Mustafagcmrl@users.noreply.github.com>
2023-01-16 13:33:32 +0300

    Add files via upload

---
+++ rationalNumber/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Rational rational = new Rational(8,9);
+        System.out.println(rational.getResult());
+
+        Rational rational1 = new Rational(8,0);
+        System.out.println(rational1.getResult());
+
+    }
+
+}
---
+++ rationalNumber/src/main/java/Rational.java
@@ -0,0 +1,70 @@
+public class Rational implements Comparable<Rational>{
+
+    private static int numberOfObjects = 0;
+    private int numerator;
+    private int denominator;
+    private double result;
+
+    public Rational(int numerator, int denominator){ //constructor
+       this.numerator = 9;
+       this.denominator = 3;
+       result = (double)numerator/denominator;
+       numberOfObjects += 1;
+    }
+
+    @Override
+    public int compareTo(Rational o) { //Method Comparable interface'inden geliyor fakat override ederek içini doldurmak zorundayız.
+
+        if(result == o.getResult()){ //Karşılaştırılan objedeki result değeriyle eşitse "0" döndür.
+            return 0;
+        }
+        else if(result > o.getResult()){ //Karşılaştırılan objedeki result değerinden büyükse "1" döndür.
+            return 1;
+        }
+        else{ //Karşılaştırılan objedeki result değerinden küçükse "-1" döndür.
+            return -1;
+        }
+
+    }
+
+
+    public void SetNumerator(int value){ //sets numerator
+        numerator = value;
+        setResult();
+    }
+
+    public void SetDenominator(int value){ //sets denominator
+        denominator = value;
+        setResult();
+    }
+
+    public int GetNumerator(){ //returns numerator
+        return numerator;
+    }
+
+    public int GetDenominator(){ //returns denominator
+        return denominator;
+    }
+
+    public double getResult(){ //Sonuç objesini döndürür.
+        return result;
+    }
+
+    private void setResult(){
+        this.result = (double)numerator/denominator;
+    }
+
+    public int objectNumber(){ //Rational class'ından kaç adet obje oluşturulduğunu döndürür.
+        return numberOfObjects;
+    }
+
+
+    @Override
+    public String toString(){ //Java'da System.out.println() object class'ındaki toString methodunu çağırır.
+                              //System.out.println(rational) yazıldığında class ismi ve bir adres döndürür.
+                              //Ama bu methodu class içinde override ederek System.out.println(rational) şeklindeki
+                              //Kullanımlarda istediğimiz herhangi bir yazıyı/veriyi bastırabilir.
+        return (numerator + "/" + denominator);
+    }
+
+}
